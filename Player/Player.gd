af9c78f154d89e8d87dd5bb9ae8679f0e7544f1f
haxeil <lezharayman@gmail.com>
2023-02-04 20:10:09 +0100

    improved moevement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,31 +1,41 @@
 extends KinematicBody2D
 
 var gravity = 12
-var speed = 40
+var speed = 180
 var velocity = Vector2.ZERO
 onready var ground_ray = $Ray
 onready var ray_vector = ground_ray.cast_to
+
 
 func _physics_process(dt):
 	apply_gravity(dt);
 	move()
 
+func _process(delta):
+	adjust_movement()
 
 func apply_gravity(dt):
 	if ground_ray.is_colliding():
-		velocity.y = 0
+		velocity = Vector2(cos(rotation), sin(rotation)) * speed
+		if rotation == 0:
+			velocity.y = 0
 	else:
 		velocity.y += gravity;
 
 func move():
-	velocity.x = speed;
-	move_and_slide(velocity, Vector2.UP)
+
+	move_and_slide(velocity, ray_vector)
+
+func adjust_movement():
 	if ground_ray.is_colliding():
 		var normal_vec = ground_ray.get_collision_normal()
-		if normal_vec.cross(ray_vector) == 0: return
 		var adjustment_angle = -normal_vec.angle_to(-ray_vector)
 		if abs(adjustment_angle) >= deg2rad(80): return
 		var tween = get_tree().create_tween()
 		tween.tween_property(self, "rotation", adjustment_angle, 0.3)
 
 
+
+	
+	
+	
